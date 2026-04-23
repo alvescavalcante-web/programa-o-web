0b88ec86ab38bfb4140da66bab74f043e2f04107
alvescavalcante-web <alves.cavalcante@estudante.ifmt.edu.br>
2026-04-22 23:07:33 -0400

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,11 @@
+let nome ="nome"
+let idade =24
+let curso ="TSI"
+let valorlogico =true
+
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof curso)
+console.log(typeof valorlogico)
+
