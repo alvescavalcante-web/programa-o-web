fa315444c36b1e4017d9c7acc0aa774cb69eb032
alvescavalcante-web <alves.cavalcante@estudante.ifmt.edu.br>
2026-04-29 20:41:21 -0400

    Add files via upload

---
+++ exercicio5.js
@@ -0,0 +1,13 @@
+let nota = 7
+
+if (nota >= 7){
+    console.log("aprovado");
+}else if (nota >= 5){
+    console.log("recuperação");
+}else{
+    console.log("reprovado");
+}
+
+
+
+
---
+++ exercicio6.js
@@ -0,0 +1,18 @@
+let nivel = 1;
+switch (nivel) {
+case 1:
+    console.log("iniciante");
+    break;
+case 2:
+    console.log("intermediario");
+    break;
+case 3:   
+    console.log("avançado");
+    break;
+}
+
+
+
+
+
+
